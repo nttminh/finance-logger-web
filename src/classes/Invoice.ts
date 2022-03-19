bd62bd5536f44808e48760cea39691e70527f50b
nttminh-dev <mnguyen@bhcc.edu>
2022-03-18 22:01:47 -0400

    created invoice class

---
+++ src/classes/Invoice.ts
@@ -0,0 +1,12 @@
+import { HasFormatter } from '../interfaces/HasFormatter';
+export class Invoice implements HasFormatter {
+	constructor(
+		readonly client: string,
+		private details: string,
+		public amount: number
+	) {}
+
+	format(): string {
+		return `${this.client} owes $${this.amount} for ${this.details}`;
+	}
+}
